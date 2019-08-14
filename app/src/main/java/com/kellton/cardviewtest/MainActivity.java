01d0c78bf936b70f53d7cb204d0cb94cfdabe8b5
kellton <nayanmeher16@gmail.com>
2019-08-14 14:43:41 +0530

    Initial commit

---
+++ app/src/main/java/com/kellton/cardviewtest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.kellton.cardviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cardView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardView1= findViewById(R.id.card_view);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "CardView1", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
