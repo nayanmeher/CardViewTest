3e673650f5ff2c32faff4dc02b0d4445240d4a25
kellton <nayanmeher16@gmail.com>
2019-08-14 14:45:43 +0530

    added log message

--- app/src/main/java/com/kellton/cardviewtest/MainActivity.java
+++ app/src/main/java/com/kellton/cardviewtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.kellton.cardviewtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "CardViewtest");
 
         cardView1= findViewById(R.id.card_view);
         cardView1.setOnClickListener(new View.OnClickListener() {
